0a69ed482f9b55f5426d304db8f2528f2790c173
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-05-30 13:35:21 +0000

    Fixed bugs

--- javascript/script.js
+++ javascript/script.js
@@ -9,28 +9,29 @@ $('a').mouseleave(function(){
     $(this).css("color", "#4a4a4f");
 });
 
+
+
+
 $(".card").mouseenter(function(){
     $(this).add(".cardTitle").css("background-color", "#92a48c").css("color", "white");
-    $(".btn-default").css("background-color", "white").css("color", "#92a48c");
+  
     
 });
 
 $(".card").mouseleave(function(){
     $(this).add(".cardTitle").css("background-color", "white").css("color", "#4a4a4f");
-    $(".btn-default").css("background-color", "#92a48c").css("color", "white");
     $(".cardTitle").css("background-color", "white").css("color", "#5EC5C4")
    
 });
 
 $(".categoryCard2").mouseenter(function(){
     $(this).add(".cardTitle").css("background-color", "#5EC5C4").css("color", "white");
-    $(".btn-default").css("background-color", "white").css("color", "#92a48c");
+    
     
 });
 
 $(".categoryCard2").mouseleave(function(){
     $(this).add(".cardTitle").css("background-color", "white").css("color", "#4a4a4f");
-    $(".btn-default").css("background-color", "#92a48c").css("color", "white");
     $(".cardTitle").css("background-color", "white").css("color", "#92a48c")
    
 });
